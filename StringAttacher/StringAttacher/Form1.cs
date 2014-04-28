f5196683e0a14f87b92bac2455cf49742be8cd74
Odinion <usonchen@gmail.com>
2014-04-28 18:14:05 +0800

    Initial Create StringAttacher
    Initial Create StringAttacher

---
+++ StringAttacher/StringAttacher/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringAttacher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String resultString = textBoxString2Add.Text + textBoxOriginal.Text;
+            Clipboard.SetText(resultString);
+            label3.Text = "处理完成";
+        }
+
+        private void textBoxOriginal_TextChanged(object sender, EventArgs e)
+        {
+            label3.Text = "";
+        }
+
+        private void textBoxString2Add_TextChanged(object sender, EventArgs e)
+        {
+            label3.Text = "";
+        }
+    }
+}
